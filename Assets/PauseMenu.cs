78eca8296ff7ed48c915035b01aeca00f1b62b1c
guillelm <33108366+guillelm@users.noreply.github.com>
2020-02-16 20:20:22 -0800

    Practica #2
    Se implemento la practica 2 de DDI

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool IsPaused = false;
+    public GameObject pauseMenuUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(IsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        IsPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        IsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("Loading Menu..");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Quitting Game..");
+    }
+}
